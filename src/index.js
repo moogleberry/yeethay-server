7b433fc99330ac1967c4bf91fe4fabeca782f844
moogleberry <uremog@gmail.com>
2020-07-08 22:45:53 +0100

    serving static files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
 
 const app = express();
 const port = 3001;
 
+let corsOptions = {
+	"origin": "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use('/static', express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
