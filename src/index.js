941dcdc2e24b4598e19b69bde90c2b3a331fe860
moogleberry <uremog@gmail.com>
2020-07-09 03:44:55 +0100

    now connects to database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 const port = 3001;
@@ -9,10 +10,56 @@ let corsOptions = {
 	"origin": "http://localhost:3000"
 };
 
-app.use(cors(corsOptions));
+MongoClient.connect('mongodb://localhost:27017/yeethay', function(err, client) {
+	if(err) throw err;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+	let db = client.db('yeethay');
 
-app.use('/static', express.static('public'));
+	app.use(cors(corsOptions));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+	app.get("/", (req, res) => res.send("Hello World!"));
+
+	app.get("/rest/ratingOptions", (req, res) => {
+		db.collection('rating_options').find({}, {
+			"sort": {
+				"value": 1
+			}
+		}).toArray(function (err, result) {
+			if (err) throw err;
+			res.send(result);
+		});
+	});
+
+	app.get("/rest/getItem", (req, res) => {
+		db.collection('rating_items').find(req.query, {
+			"limit": 1
+		}).next(function (err, result) {
+			if (err) throw err;
+			res.send(result);
+		});
+	});
+
+	app.get("/rest/getItems", (req, res) => {
+		db.collection('rating_items').find(req.query).toArray(function (err, result) {
+			if (err) throw err;
+			res.send(result);
+		});
+	});
+
+	app.get("/rest/unratedItem", (req, res) => {
+		db.collection('rating_items').find({
+			"name": "Lychee"
+		}, {
+			"limit": 1
+		}).next(function (err, result) {
+			if (err) throw err;
+			res.send(result);
+		});
+	});
+
+	app.listen(port, () => console.log(`Yeet Hay listening at http://localhost:${port}`));
+});
+
+
+
+
