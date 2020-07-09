c76547b81710e122a5ee3a109080358f46615bfd
moogleberry <uremog@gmail.com>
2020-07-09 19:06:26 +0100

    added random item

--- src/index.js
+++ src/index.js
@@ -20,46 +20,59 @@ MongoClient.connect('mongodb://localhost:27017/yeethay', function(err, client) {
 	app.get("/", (req, res) => res.send("Hello World!"));
 
 	app.get("/rest/ratingOptions", (req, res) => {
-		db.collection('rating_options').find({}, {
-			"sort": {
-				"value": 1
-			}
-		}).toArray(function (err, result) {
-			if (err) throw err;
-			res.send(result);
-		});
+		db.collection('rating_options')
+			.find({}, {
+				"sort": {
+					"value": 1
+				}
+			})
+			.toArray(function (err, result) {
+				if (err) throw err;
+				res.send(result);
+			});
 	});
 
 	app.get("/rest/getItem", (req, res) => {
-		db.collection('rating_items').find(req.query, {
-			"limit": 1
-		}).next(function (err, result) {
-			if (err) throw err;
-			res.send(result);
-		});
+		db.collection('rating_items')
+			.find(req.query, {
+				"limit": 1
+			})
+			.next(function (err, result) {
+				if (err) throw err;
+				res.send(result);
+			});
 	});
 
 	app.get("/rest/getItems", (req, res) => {
-		db.collection('rating_items').find(req.query).toArray(function (err, result) {
-			if (err) throw err;
-			res.send(result);
-		});
+		db.collection('rating_items')
+			.find(req.query)
+			.toArray(function (err, result) {
+				if (err) throw err;
+				res.send(result);
+			});
 	});
 
 	app.get("/rest/unratedItem", (req, res) => {
-		db.collection('rating_items').find({
-			"name": "Lychee"
-		}, {
-			"limit": 1
-		}).next(function (err, result) {
-			if (err) throw err;
-			res.send(result);
-		});
+		db.collection('rating_items')
+			.find({
+				"name": "Lychee"
+			}, {
+				"limit": 1
+			})
+			.next(function (err, result) {
+				if (err) throw err;
+				res.send(result);
+			});
+	});
+
+	app.get("/rest/randomItem", (req, res) => {
+		db.collection('rating_items')
+			.aggregate([{ $sample: { size: 1 } }])
+			.next(function (err, result) {
+				if (err) throw err;
+				res.send(result);
+			});;
 	});
 
 	app.listen(port, () => console.log(`Yeet Hay listening at http://localhost:${port}`));
 });
-
-
-
-
