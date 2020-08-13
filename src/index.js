0d7b4ddb9ce64bcbdd3438130d697e8b8e8224e5
moogleberry <uremog@gmail.com>
2020-08-13 21:17:49 +0100

    uses a promise

--- src/index.js
+++ src/index.js
@@ -10,75 +10,75 @@ let corsOptions = {
 	"origin": "http://localhost:3000"
 };
 
-MongoClient.connect('mongodb://localhost:27017/yeethay', function(err, client) {
-	if(err) throw err;
+MongoClient.connect('mongodb://localhost:27017/yeethay')
+	.then((client) => {
+		let db = client.db('yeethay');
 
-	let db = client.db('yeethay');
+		app.use(cors(corsOptions));
+		app.use(express.json());
 
-	app.use(cors(corsOptions));
-	app.use(express.json());
+		app.get("/", (req, res) => res.send("Hello World!"));
 
-	app.get("/", (req, res) => res.send("Hello World!"));
+		app.get("/rest/ratingOptions", (req, res) => {
+			db.collection('rating_options')
+				.find({}, {
+					"sort": {
+						"value": 1
+					}
+				})
+				.toArray(function (err, result) {
+					if (err) throw err;
+					res.send(result);
+				});
+		});
 
-	app.get("/rest/ratingOptions", (req, res) => {
-		db.collection('rating_options')
-			.find({}, {
-				"sort": {
-					"value": 1
-				}
-			})
-			.toArray(function (err, result) {
-				if (err) throw err;
-				res.send(result);
-			});
-	});
+		app.get("/rest/getItem", (req, res) => {
+			db.collection('rating_items')
+				.find(req.query, {
+					"limit": 1
+				})
+				.next(function (err, result) {
+					if (err) throw err;
+					res.send(result);
+				});
+		});
 
-	app.get("/rest/getItem", (req, res) => {
-		db.collection('rating_items')
-			.find(req.query, {
-				"limit": 1
-			})
-			.next(function (err, result) {
-				if (err) throw err;
-				res.send(result);
-			});
-	});
+		app.get("/rest/getItems", (req, res) => {
+			db.collection('rating_items')
+				.find(req.query)
+				.toArray(function (err, result) {
+					if (err) throw err;
+					res.send(result);
+				});
+		});
 
-	app.get("/rest/getItems", (req, res) => {
-		db.collection('rating_items')
-			.find(req.query)
-			.toArray(function (err, result) {
-				if (err) throw err;
-				res.send(result);
-			});
-	});
+		app.get("/rest/unratedItem", (req, res) => {
+			db.collection('rating_items')
+				.find({
+					"name": "Lychee"
+				}, {
+					"limit": 1
+				})
+				.next(function (err, result) {
+					if (err) throw err;
+					res.send(result);
+				});
+		});
 
-	app.get("/rest/unratedItem", (req, res) => {
-		db.collection('rating_items')
-			.find({
-				"name": "Lychee"
-			}, {
-				"limit": 1
-			})
-			.next(function (err, result) {
-				if (err) throw err;
-				res.send(result);
-			});
-	});
+		app.get("/rest/randomItem", (req, res) => {
+			db.collection('rating_items')
+				.aggregate([{ $sample: { size: 1 } }])
+				.next(function (err, result) {
+					if (err) throw err;
+					res.send(result);
+				});
+		});
 
-	app.get("/rest/randomItem", (req, res) => {
-		db.collection('rating_items')
-			.aggregate([{ $sample: { size: 1 } }])
-			.next(function (err, result) {
-				if (err) throw err;
-				res.send(result);
-			});
-	});
+		app.put("/rest/rateItem", (req, res) => {
+			console.log(req.body);
+			res.send(req.body);
+		});
 
-	app.put("/rest/rateItem", (req, res) => {
-		console.log(req.body);
-		res.send(req.body);
-	});
-
-	app.listen(port, () => console.log(`Yeet Hay listening at http://localhost:${port}`));
-});
+		app.listen(port, () => console.log(`Yeet Hay listening at http://localhost:${port}`));
+	})
+	.catch((err) => console.log("ERROR", err));
